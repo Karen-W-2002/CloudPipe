7b131aa3c7eb97544f8ac0f451afca0c59345713
Karen-W-2002 <karenweidev@gmail.com>
2026-06-04 16:12:06 +0800

    Add iam ec2 role with ECRReadOnly

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,26 @@ resource "aws_instance" "pyflask_ec2" {
               sudo usermod -aG docker ec2-user
               EOF
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "pyflask-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_role_attachment" {
+  role = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicReadOnly"
+}
