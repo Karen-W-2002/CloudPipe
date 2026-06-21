8a822cc964e69374f89e9d06f82050cae6d57841
Karen-W-2002 <karenweidev@gmail.com>
2026-06-21 21:28:25 +0800

    Add state locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "pyflask-tfstate"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true // Enable state locking to prevent concurrent modifications
   }
 }
